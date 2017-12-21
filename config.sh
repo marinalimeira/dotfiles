d7c033791adede76f0dcd2292a478401b2790f93
Marina Limeira <marinaflessa@gmail.com>
2017-12-21 09:21:39 -0200

    Update vim install config

--- config.sh
+++ config.sh
@@ -1,4 +1,6 @@
-mkdir ~/.config
+# install plug - a minimalist Vim plugin manager.
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # forces creation of a link
 ln -f config/nvim/init.vim ~/.config/nvim/init.vim
